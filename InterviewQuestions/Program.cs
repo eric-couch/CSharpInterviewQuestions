d1d368215d996f47bea78aa2bea0c17b0922f74a
Eric Couch <james.couch@ruminatetech.com>
2019-12-16 22:29:34 -0600

    added longest word solution

--- InterviewQuestions/Program.cs
+++ InterviewQuestions/Program.cs
@@ -55,6 +55,24 @@ namespace InterviewQuestions
             Console.WriteLine("max = " + max);
             Console.WriteLine("min = " + min);
         }
+
+        // give a string, find the longet word in the string and return it
+        public static string FindLongestWord(string para)
+        {
+            int longestWordLength = 0;
+            String longestWord = "";
+            String[] words = para.Split(" ");
+
+            for (int i = 0; i<words.Length; i++)
+            {
+                if (words[i].Length > longestWordLength)
+                {
+                    longestWordLength = words[i].Length;
+                    longestWord = words[i];
+                } 
+            }
+            return longestWord;
+        }
         static void Main(string[] args)
         {
             int[] numbers = { 0, 1, 2, 35, 6, 7, 8, 44, 66, 3, 9, -7 };
@@ -62,6 +80,9 @@ namespace InterviewQuestions
             LargestSmallest(numbers);
 
             PrintSquare(15);
+
+            String testString = "Now is the time for all thisisatest good men to come to the aid of their country.";
+            Console.WriteLine(FindLongestWord(testString));
         }
     }
 }
