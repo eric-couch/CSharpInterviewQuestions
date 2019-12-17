7b226c1e6e6b31f40caa8fb0b0687baa06b15930
Eric Couch <james.couch@ruminatetech.com>
2019-12-17 12:45:02 -0600

    more problems

--- InterviewQuestions/Program.cs
+++ InterviewQuestions/Program.cs
@@ -54,15 +54,13 @@ namespace InterviewQuestions
 
         public static string FindLongestWord(string para)
         {
-            int longestWordLength = 0;
             String longestWord = "";
             String[] words = para.Split(" ");
 
             for (int i = 0; i < words.Length; i++)
             {
-                if (words[i].Length > longestWordLength)
+                if (words[i].Length > longestWord.Length)
                 {
-                    longestWordLength = words[i].Length;
                     longestWord = words[i];
                 }
             }
@@ -82,32 +80,68 @@ namespace InterviewQuestions
             {
                 for (int v=1; v<=length; v++)
                 {
-                    if (h==1 || h==length)
+                    if (h==1 || h==length || v == 1 || v == length)
                     {
                         Console.Write("*");
-                    } else 
+                    } else
                     {
-                        if (v==1|| v==length)
-                        {
-                            Console.Write("*");
-                        } else
-                        {
-                            Console.Write(" ");
-                        }
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
             }
         }
+        
+        public static String ReturnSquare(int length)
+        {
 
+            String square = "";
+            for (int h = 1; h <= length; h++)
+            {
+                for (int v = 1; v <= length; v++)
+                {
+                    if (h == 1 || h == length || v ==1 || v == length)
+                    {
+                        square += "*";
+                    }
+                    else
+                    {
+                        square += " ";
+                    }
+                }
+                square += "\n";
+            }
+            return square;
+        }
+        public static String ReturnSquareSimple(int length)
+        {
+            String square = new String('*', length) + "\n";
+            for (int h = 1; h <= length-2; h++)
+            {
+                square += "*" + new String(' ', length - 2) + "*\n";
+            }
+            square += new String('*', length);
+            return square;
+        }
         public static void PrintTree(int height)
         {
+            // from one to height, that's the number of lines = height
             for (var h=1;h<height-1;h++)
             {
+                //   *
+                //  ***
+                // *****
+                //*******
+                // first inner loop is spaces
+                // how many spaces line one?  Three, so start at zero
+                // w=0, go to (4-1)=3, 3-2=1
+
                 for (var w=0;w<(height-h)-2;w++)
                 {
                     Console.Write(" ");
                 }
+
+                // second loop is *
                 for (var w=0;w<(h*2)-1;w++)
                 {
                     Console.Write("*");
@@ -125,6 +159,15 @@ namespace InterviewQuestions
             return initials;
             
         }
+        public static String ReverseString (String str)
+        {
+            String rev = "";
+            for (int i= str.Length-1; i>=0;i--)
+            {
+                rev += str[i];
+            }
+            return rev;
+        }
         static void Main(string[] args)
         {
 
@@ -139,11 +182,15 @@ namespace InterviewQuestions
 
             PrintSquare(15);
 
+            Console.WriteLine(ReturnSquare(10));
+
+            Console.WriteLine(ReturnSquareSimple(10));
+
             PrintTree(16);
 
             Console.WriteLine(GetInitials("James Eric Couch"));
 
-            
+            Console.WriteLine(ReverseString("James Eric Couch"));
         }
     }
 }
