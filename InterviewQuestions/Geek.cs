46c3ad0c32d4fc1cd3bfbabaaf29ca591a4f82ed
Eric Couch <james.couch@ruminatetech.com>
2019-12-17 08:29:27 -0600

    added hashteable and constructor

---
+++ InterviewQuestions/Geek.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewQuestions
+{
+    class Geek
+    {
+        int IQ { get; set; }
+        // Constructor
+        public Geek(int iq) {
+            this.IQ = iq;
+        }
+
+        static void Main(string[] args)
+        {
+            // an object is created of Geek class,
+            // So above constructor is called
+            Geek eric = new Geek(160);
+
+        }
+    }
+}
